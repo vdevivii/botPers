9c85183e6240d0065c33c19757aa4d15011eb011
Vivian Revollo <revollovivian@gmail.com>
2024-08-19 22:55:49 -0400

    Genero

--- src/bot.js
+++ src/bot.js
@@ -1,3 +1,15 @@
-export default function respuesta(name) {
+export function respuesta(name) {
     return `Hola, ${name}! Bienvenido.`;
+}
+
+export function handleNameInput(name) {
+    return `Hola, ${name}. Ahora, ¿cuál es tu género? (M para Mujer, H para Hombre)`;
+}
+
+export function handleGenderInput(name, gender) {
+    if (gender === 'M') {
+        return `Hola, señora ${name}.`;
+    } else if (gender === 'H') {
+        return `Hola, señor ${name}.`;
+    }
 }
--- src/presenter.js
+++ src/presenter.js
@@ -1,19 +1,44 @@
-import respuesta from './bot.js';
+import { respuesta, handleNameInput, handleGenderInput } from './bot.js';
 
 document.addEventListener("DOMContentLoaded", () => {
     const form = document.querySelector("#bot-form");
+    const nameSection = document.querySelector("#name-section");
+    const genderSection = document.querySelector("#gender-section");
     const nameInput = document.querySelector("#name-input");
+    const genderInput = document.querySelector("#gender-input");
     const responseDiv = document.querySelector("#resp-div");
+
+
+    const form2 = document.querySelector("#bot2-form");
+    const responseDivG = document.querySelector("#respgender-div");
+
+    let userName = '';
 
     form.addEventListener("submit", (event) => {
         event.preventDefault();
 
-        const name = nameInput.value.trim(); // Obtenemos el valor del input y eliminamos los espacios en blanco
-
-        if (name) {
-            responseDiv.innerHTML = `<p>${respuesta(name)}</p>`;
-        } else {
-            responseDiv.innerHTML = "<p>Por favor, introduce tu nombre.</p>";
-        }
+        if (nameSection.style.display !== 'none') {
+            userName = nameInput.value.trim();
+            if (userName) {
+                responseDiv.innerHTML = `<p>${handleNameInput(userName)}</p>`;
+                nameSection.style.display = 'none';
+                genderSection.style.display = 'block';
+            } else {
+                responseDiv.innerHTML = "<p>Por favor, introduce tu nombre.</p>";
+            }
+        
+          }
     });
+
+
+form2.addEventListener("submit", (event) => {
+if (genderSection.style.display !== 'none') {
+  const gender = genderInput.value.trim().toUpperCase();
+  if (gender === 'M' || gender === 'H') {
+    responseDivG.innerHTML = `<p>${handleGenderInput(userName, gender)}</p>`;
+  } else {
+    responseDivG.innerHTML = "<p>Por favor, introduce un género válido (M o H).</p>";
+  }
+}
+});
 });
