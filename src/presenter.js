b5f2a81fec79913d0c30bd6e811c90f4698519f2
Vivian Revollo <revollovivian@gmail.com>
2024-08-20 12:05:44 -0400

    iDIOMAS AÑADIDOS

--- src/presenter.js
+++ src/presenter.js
@@ -4,10 +4,14 @@ document.getElementById('bot-form').addEventListener('submit', function(event) {
   const name = document.getElementById('name-input').value.trim();
   const age = parseInt(document.getElementById('age-input').value.trim(), 10);
   const gender = document.getElementById('gender-input').value.trim().toUpperCase();
+  const leng = document.getElementById('leng-input').value.trim().toUpperCase();
+
   const responseDiv = document.getElementById('resp-div');
 
   let greeting;
 
+  if(leng == "SP")
+    {
   if (age >= 20) {
     if (gender === "M") {
       greeting = `Hola, señor ${name}`;
@@ -25,6 +29,31 @@ document.getElementById('bot-form').addEventListener('submit', function(event) {
       greeting = "Género no reconocido, por favor use M o F.";
     }
   }
+}
+  else if (leng == "ENG")
+  {
+
+    if (age >= 20) {
+      if (gender === "M") {
+        greeting = `Hello, sir ${name}`;
+      } else if (gender === "F") {
+        greeting = `Hello, madam ${name}`;
+      } else {
+        greeting = "Gender not found, please use M or F.";
+      }
+    } else {
+      if (gender === "M") {
+        greeting = `Hi, bro ${name}`;
+      } else if (gender === "F") {
+        greeting = `Hi, lady ${name}`;
+      } else {
+        greeting = "Gender not found, please use M or F.";
+      }
+
+  }
+}
+
 
   responseDiv.textContent = greeting;
+
 });
